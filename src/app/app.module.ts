7ea35ca96bed8bd8281566b5532ef038ec70e9b9
lucaseblock <lucasblock049@gmail.com>
2020-07-11 18:04:15 -0300

    Fin de la sección 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,16 @@ import { APP_ROUTES } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-import { PagesModule } from './pages/pages.module';
 
+// modulos
+import { PagesModule } from './pages/pages.module';
 
 
 // temporal
 import { FormsModule } from '@angular/forms';
 
-
-//modulos
-
+// servicios
+import { ServiceModule } from './services/service.module';
 
 
 @NgModule({
@@ -27,7 +27,8 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     APP_ROUTES,
     PagesModule,
-    FormsModule
+    FormsModule,
+    ServiceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
